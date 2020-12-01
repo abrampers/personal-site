ab3570085f36e6ddbdaae24783a76a7e192174ac
Abram Perdanaputra Situmorang <abram.perdanaputra@gmail.com>
2020-12-02 06:54:14 +0700

    Add setup user email and name on deploy script

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,10 @@ cd public
 
 git add .
 
+# Fill in credentials
+git config user.name $USER_NAME
+git config user.email $USER_EMAIL
+
 # Commit changes.
 msg="rebuilding site HUGO:$(date)"
 if [ -n "$*" ]; then
