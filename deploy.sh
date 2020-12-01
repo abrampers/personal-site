f4d9f2f134bdba4a06dab38a7afcad459bc32969
Abram Situmorang <abram.perdanaputra@gmail.com>
2020-12-01 18:50:31 +0700

    Start migration to blog.abram.id

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# If a command fails then the deploy stops
+set -e
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+# Build the project.
+hugo -t anubis
+
+cd public
+
+git add .
+
+# Commit changes.
+msg="rebuilding site HUGO:$(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
+
+git push origin master
